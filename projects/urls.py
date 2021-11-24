359c366b35986259198e965b8344421e18902e60
gooyacoder <gooyacoder@gmail.com>
2021-11-24 11:54:03 +0330

    Up to Video 05_01

--- projects/urls.py
+++ projects/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.projects, name='projects'),
     path('project/<str:pk>/', views.project, name='project'),
     path('create_project', views.createProject, name='create-project'),
+    path('update_project/<str:pk>', views.updateProject, name='update-project'),
+    path('delete_project/<str:pk>', views.deleteProject, name='delete-project'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
 from django.http import HttpResponse
 from .forms import ProjectForm
@@ -17,5 +17,32 @@ def project(request, pk):
 
 def createProject(request):
     form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
     context = {'form': form}
     return render(request, 'project_form.html', context)
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'project_form.html', context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'project': project}
+    return render(request, 'delete_object.html', context)
